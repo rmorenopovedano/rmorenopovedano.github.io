eb89bda1cbe2bd94fe4af8de342e2141458c931d
Raul Moreno <Raul Moreno>
2016-02-15 20:00:35 +0100

    jQueryUI

---
+++ jQueryUI/Dialog/dialog.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Raul on 15/02/2016.
+ */
+$(function() {
+      $( "#dialog" ).dialog();
+});
---
+++ jQueryUI/Tooltip/tooltip.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Raul on 15/02/2016.
+ */
+$(function() {
+    var tooltips = $( "[title]" ).tooltip({
+        position: {
+            my: "left top",
+            at: "right+5 top-5"
+        }
+    });
+});
